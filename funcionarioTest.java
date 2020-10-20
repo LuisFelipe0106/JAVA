220998632da477fddb95a73bedaa15d125669c52
LuisFelipe0106 <48720207+LuisFelipe0106@users.noreply.github.com>
2020-10-20 18:52:30 -0300

    Add files via upload

---
+++ funcionarioTest.java
@@ -0,0 +1,9 @@
+package exercicio1;
+
+public class FuncionarioTest {
+
+    public static void main(String[] args) {
+        Funcionario F1 = new Funcionario();
+        F1.salario = 2000;
+    }
+}
